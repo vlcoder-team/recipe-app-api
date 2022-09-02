d555286cf5c58bc865ab15277b70d7a7ae72445a
Dinh Van Luc <dinhvanluc69@gmail.com>
2022-09-02 23:01:12 +0700

    Fixed typing

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database"""
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
         pass
