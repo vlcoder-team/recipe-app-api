2ff9c9c7f6c24cea6dd7777371215fdca4a9d41f
Dinh Van Luc <dinhvanluc69@gmail.com>
2022-09-02 23:27:34 +0700

    Fixed typing

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Wating for database...')
+        self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
