7e79d79a42d74881e0acb1514e16c1d49f0033c3
Dinh Van Luc <dinhvanluc69@gmail.com>
2022-09-04 23:07:51 +0700

    Fixed typos error

--- app/core/models.py
+++ app/core/models.py
@@ -25,8 +25,8 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
-        user.is_superuser = True
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
