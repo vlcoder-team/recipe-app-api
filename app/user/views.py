d3ec0192adfae8f9a2f4c2652fd1b9742b386739
MrL8199 <dinhvanluc69@gmail.com>
2023-01-27 16:34:59 +0700

    Typo

--- app/user/views.py
+++ app/user/views.py
@@ -22,7 +22,7 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManagerUserView(generics.RetrieveUpdateAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
